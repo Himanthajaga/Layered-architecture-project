2950bef3fac1e7b4afab7ee3808d01accd963cd8
J.G Himantha <himanthagamachchige@gmail.com>
2024-06-18 14:02:14 +0530

    placeorder done

--- Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
+++ Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -3,7 +3,6 @@ package com.example.layeredarchitecture.dao;
 
 import com.example.layeredarchitecture.model.CustomerDTO;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
--- Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/ItemDAOImpl.java
+++ Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/ItemDAOImpl.java
@@ -1,9 +1,9 @@
 package com.example.layeredarchitecture.dao;
 
-import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.ItemDTO;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemDAOImpl implements ItemDAO{
